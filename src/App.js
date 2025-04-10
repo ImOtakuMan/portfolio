9fafeb47dd3bedfba7f725b48ec35eee09ed6b64
Kitol <kitoltd18@gmail.com>
2025-04-10 16:15:51 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  <h1>My Portfolio</h1>
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+        <p>Welcome to my portfolio website!</p>
+        <p>Here you can find my projects and skills.</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
