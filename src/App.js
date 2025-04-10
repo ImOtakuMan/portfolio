d91f3b81d331d2e0880c4ac04042f282738558ac
Kitol <kitoltd18@gmail.com>
2025-04-10 16:25:31 +0200

    Main page test changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
         <h1>My Portfolio</h1>
         <p>Welcome to my portfolio website!</p>
         <p>Here you can find my projects and skills.</p>
+        <h2>Projects</h2>
+        <ul>
+          <li>Project 1: My first project</li>
+          <li>Project 2: My second project</li>
+          <li>Project 3: My third project</li>
+        </ul>
       </header>
     </div>
   );
